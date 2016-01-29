4f55f166fb5fc83e55f01c0d948df7f4315eab88
Christopher Baker <me@christopherbaker.net>
2016-01-28 23:04:51 -0600

    Add addon_config.mk.

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxUnicode
+	ADDON_DESCRIPTION = A set of Unicode tools for openFrameworks.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "unicode" "text"
+	ADDON_URL = http://github.com/bakercp/ofxUnicode
