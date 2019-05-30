0a58bb8512dbebb1214e9a0ec53ec95c94e349c4
Christopher Baker <info@christopherbaker.net>
2019-05-30 15:44:22 -0600

    Update addon_config.mk for libunibreak on Linux*.

--- addon_config.mk
+++ addon_config.mk
@@ -4,3 +4,13 @@ meta:
 	ADDON_AUTHOR = bakercp
 	ADDON_TAGS = "unicode" "text"
 	ADDON_URL = http://github.com/bakercp/ofxUnicode
+
+linux:
+	ADDON_PKG_CONFIG_LIBRARIES = libunibreak 
+linux64:
+	ADDON_PKG_CONFIG_LIBRARIES = libunibreak 
+linuxarmv6l:
+	ADDON_PKG_CONFIG_LIBRARIES = libunibreak 
+linuxarmv7l:
+	ADDON_PKG_CONFIG_LIBRARIES = libunibreak 
+
